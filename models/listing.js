167449cfeac9caaa62fcff9d7a01a0e92dbd540d
myst93 <parth.oberai39@gmail.com>
2025-07-17 23:05:20 +0530

    hh

--- models/listing.js
+++ models/listing.js
@@ -37,8 +37,8 @@ const ListingSchema = new Schema({
     },
     description: String,
     image: {
-        filename: String,
-        url: String
+        filename: { type: String, default: "listingimage" },
+        url: { type: String, default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60" }
     },
     price: Number,
     location: String,
