e557098f3dbb6f82489a71028864a9ec32fb6efd
myst93 <parth.oberai39@gmail.com>
2025-07-17 22:54:15 +0530

    Still working on the Airbnb project

--- models/listing.js
+++ models/listing.js
@@ -1,3 +1,32 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const ListingSchema = new Schema({
+//     title: {
+//         type: String,
+//         required: true
+//     },
+//     description: String,
+//     image: {
+//     type: String,
+//     default:
+//       "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+//     set: (v) =>
+//       v === ""
+//         ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+//         : v,
+//   },
+//     price: Number,
+//     location: String,
+//     country: String
+// });
+
+
+// const Listing =mongoose.model('Listing', ListingSchema)// yeh Listing model ko create karta h jo ki ListingSchema ke according h
+
+// module.exports = Listing;// yeh Listing model ko export karta h taaki dusre files mein use kiya ja sake
+
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -8,19 +37,13 @@ const ListingSchema = new Schema({
     },
     description: String,
     image: {
-        type:String,
-        default:"https://create.vista.com/unlimited/stock-photos/198104072/stock-photo-view-swiss-alps-raetikon-maienfeld-switzerland/",
-        set: (v) =>
-            v=== ""
-              ?"https://create.vista.com/unlimited/stock-photos/198104072/stock-photo-view-swiss-alps-raetikon-maienfeld-switzerland/"
-              : v,
+        filename: String,
+        url: String
     },
     price: Number,
     location: String,
     country: String
 });
 
-
-const Listing =mongoose.model('Listing', ListingSchema)// yeh Listing model ko create karta h jo ki ListingSchema ke according h
-
-module.exports = Listing;// yeh Listing model ko export karta h taaki dusre files mein use kiya ja sake
+const Listing = mongoose.model('Listing', ListingSchema);
+module.exports = Listing;
